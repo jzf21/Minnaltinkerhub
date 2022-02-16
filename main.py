b9d43b8f82835289f64a875144dcbda791713a8e
jzf21 <jozefneelamkavil2003@gmail.com>
2022-02-16 22:29:00 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,12 @@
+import pygame
+import math
+#initialise game
+pygame.init()
+#very important bracket inside bracket
+screen=pygame.display.set_mode((800,600))
+running=True
+while running:
+    
+
+
+
