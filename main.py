7f8a070eb58a19dd284c60fd0a2c8230344d048d
jzf21 <jozefneelamkavil2003@gmail.com>
2022-02-18 20:04:08 +0530

    post done

--- main.py
+++ main.py
@@ -1,12 +1,26 @@
 import pygame
 import math
 #initialise game
+WIDTH, HEIGHT= 700,500
+
 pygame.init()
 #very important bracket inside bracket
-screen=pygame.display.set_mode((800,600))
-running=True
-while running:
-    
+WIN=pygame.display.set_mode((WIDTH,HEIGHT))
+pygame.display.set_caption("pong")
+
+def main():
+    run=True
+    while run:
+        for event in pygame.event.get():
+            if event.type== pygame.QUIT:
+                run=False
+                break
+        
+    pygame.quit()
 
 
 
+
+
+if __name__== '__main__':
+    main()
